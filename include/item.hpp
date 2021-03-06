5b6ed293aaafe2c8e8ece859d752c695026421c9
Charles Alaras <charles.alaras@gmail.com>
2021-03-05 21:48:43 -0800

    Add printItem() & getType()
    * inv-interface needs this to print items, specifically consumables

--- include/item.hpp
+++ include/item.hpp
@@ -9,6 +9,21 @@ private:
    int effect; // How much does it heal, protect, or increase damage?
    int type; // Is this a weapon, armor piece, or consumable?
    int whoCanUse; // What class can use it?
+public:
+   void printItem(int i) {
+      std::string output = name + "\t" + to_string(effect);
+      if(type == 0) {
+         output += "\tWeapon";
+      }
+      if(type == 1) {
+         output += "\tArmor";
+      }
+      if(type == 2) {
+         output += "\tConsumable";
+      }
+      mvprintw(2 + i, 2, output.c_str());
+   }
+   int getType() { return type; }
 };
 
 #endif // __ITEM_HPP__
