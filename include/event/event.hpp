9566ab9c0d9f6f77088464ee3a80663081bf07f6
Charles Alaras <charles.alaras@gmail.com>
2021-03-06 21:00:53 -0800

    Add run() and helper functions
    * We will have to review if there's anything to change
    * Can be more efficient at this point in time...

--- include/event/event.hpp
+++ include/event/event.hpp
@@ -1,25 +1,34 @@
 #ifndef EVENT_HPP
 #define EVENT_HPP
 
+#include "character.hpp"
+#include <ncurses.h>
+#include <cmath> // For Rand
+#include <time.h>
+
 class EventBus;
 
 class Event {
 private:
-  EventBus* eventBus;
-  bool isActive = true;
+   int row;
+   int col;
+   int enemyNumber;
+   Enemy* enemy1;
+   Enemy* enemy2;
+   Enemy* enemy3;
+   User* user; // Pointer to user
+   WINDOW * eventBox;
+   Enemy* getAliveEnemy(); // Get best target for attack
+   int sumEnemiesHealth(); // Event ends when summed health is 0
+   int getEnemyGold(); // Sum up all Enemy gold
+   void printHP(); // Print Enemies' Health
 
 protected:
   bool finish();
 
 public:
-  Event(EventBus* eventBus);
-  
+  Event(User* maincharacter, string input);
   void run();
-  
-  bool isActive();
-
-  virtual void draw() = 0;
-  virtual void select(char option) = 0;
 
 };
 
--- src/event/event.cpp
+++ src/event/event.cpp
@@ -1,12 +1,109 @@
 #include "event/event_handler.hpp"
 #include "event/event_bus.hpp"
 
-EventHandler::EventHandler(EventBus* eventBus) : eventBus(eventBus) { }
+Event::Event(User* maincharacter, string input)
+: user(maincharacter)
+{
+   if(input == "Mountain") {
 
-void run() {
-  this->draw();
-  while(this->isActive()) {
-    //ncurses get char
-    //call this->select()
+   }
+   if(input == "Cavern") {
+
+   }
+   if(input == "Desert") {
+      
+   }
+}
+
+void Event::run() {
+  while(user->getHealth() > 0 && sumEnemiesHealth() > 0) {
+   wclear(eventBox);
+   box(eventBox, 0, 0); // drawBox
+   printHP(); // This will print the enemy HP during passing moments
+   std::pair<int, int> choice = user->getChoice(); // Get Choice from User
+   if(choice->first == 0) { // Attack
+      Enemy* target = getAliveEnemy();
+      int damageDealt = user->attack(getAliveEnemy()); // User should attack whatever enemy it can
+      std::string attackText = user->getName() + " dealt " + to_string(damageDealt) + " to " + target->getName() + " using " + user->getAttack(choice->second) + "!";
+      mvwprintw(eventBox, row / 2, 2, attackText.c_str());
+      wrefresh(eventBox);
+   }
+   if(choice->first == 1) { // Use Consumable
+      int boostGiven = user->useItem(choice->second); // Use Item at Specified Index
+      std::string boostText = user->getName() + " gained " + to_string(boostGiven) + " boost in " + user->getItemType(choice->second);
+      mvwprintw(eventBox, row / 2, 2, boostText.c_str());
+      wrefresh(eventBox);
+   }
+   if(choice->first == 2) { // Attempt Escape
+      srand(time(NULL));
+      if((rand() % 100) < 25) { // User has a 26% chance to escape the event
+         std::string runText = user->getName() + " escaped the battle successfully!";
+         mvwprintw(eventBox, row / 2, 2, runText.c_str());
+         mvwprintw(eventBox, (row / 2) + 1, 2, "Press any key to continue");
+         wrefresh(eventBox);
+         getch();
+         wclear(eventBox);
+         return;
+      }
+   }
   }
+  if(user->getHealth() <= 0) {
+   wclear(eventBox);
+   return;
+  }
+  else {
+   int goldEarned = getEnemyGold();
+   std::string successPrompt = "You won! You received " + to_string(goldEarned);
+   user->giveGold(goldEarned);
+   wclear(eventBox);
+   box(eventBox, 0, 0);
+   mvwprintw(eventBox, row / 2, 2, successPrompt.c_str());
+   wrefresh(eventBox);
+  }
+}
+
+Enemy* Event::getAliveEnemy() {
+   if(enemy1->getHealth() > 0) {
+      return enemy1;
+   }
+   if(enemy2->getHealth() > 0) {
+      return enemy2;
+   }
+   if(enemy3->getHealth() > 0) {
+      return enemy3;
+   }
+   return enemy1;
+}
+
+int Event::sumEnemiesHealth() {
+   return enemy1->getHealth() + enemy2->getHealth() + enemy3->getHealth();
+}
+
+int Event::getEnemyGold() {
+   return enemy1->getGold() + enemy2->getGold() + enemy3->getGold();
+}
+
+void Event::printHP() {
+   std::string health;
+   std::string health2;
+   std::string health3;
+   if(enemyNumber == 3) {
+      health = "HP: " + to_string(enemy1->getHealth());
+      mvwprintw(eventBox, (row / 2) - 1, 2, health);
+      health2 = "HP: " + to_string(enemy2->getHealth());
+      mvwprintw(eventBox, (row / 2) - 1, 2, health2);
+      health3 = "HP: " + to_string(enemy3->getHealth());
+      mvwprintw(eventBox, (row / 2) - 1, 2, health3);
+   }
+   if(enemyNumber == 2) {
+      health = "HP: " + to_string(enemy1->getHealth());
+      mvwprintw(eventBox, (row / 2), 2, health);
+      health2 = "HP: " + to_string(enemy2->getHealth());
+      mvwprintw(eventBox, (row / 2) - 1, 2, health2);
+   }
+   else {
+      health = "HP: " + to_string(enemy1->getHealth());
+      mvwprintw(eventBox, row / 2, 2 health);
+   }
+   wrefresh(eventBox);
 }
