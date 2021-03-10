dadf1b9ddb633498b00cf56f328a89e43ad8c3f7
SwampApe <rothvann2000@gmail.com>
2021-03-10 15:06:35 -0800

    Test files for item and skill

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
 #include "test_enemy_factory.hpp"
+#include "test_enemy.hpp"
+#include "test_item.hpp"
+#include "test_skill.hpp"
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
